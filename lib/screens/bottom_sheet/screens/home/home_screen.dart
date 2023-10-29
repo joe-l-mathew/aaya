5e92212cf40fe636a3691ab9b674ac01db2df0b2
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-10-29 23:30:26 +0530

    added Appbar demo in home

--- lib/screens/bottom_sheet/screens/home/home_screen.dart
+++ lib/screens/bottom_sheet/screens/home/home_screen.dart
@@ -6,8 +6,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text("Home"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(Icons.location_on_outlined),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("Pera 91,Kakkanad"),
+                Icon(Icons.keyboard_arrow_down_rounded),
+                Spacer(),
+                Icon(Icons.notifications_none),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(Icons.message_outlined),
+                SizedBox(
+                  width: 10,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
